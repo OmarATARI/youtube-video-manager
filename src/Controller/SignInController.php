64f889af5c9a3a0cc034bb9428b17f474c3a84b9
Omar ATARI <atari.omar@gmail.com>
2019-08-09 19:11:10 +0200

    added authentication system

--- src/Controller/SignInController.php
+++ src/Controller/SignInController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\SignInType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SignInController extends AbstractController
 {
     /**
      * @Route("/signin", name="sign_in")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('authentication/sign_up.html.twig', [
+        $user = new User();
+        $form = $this->createForm(SignInType::class, $user);
+
+        return $this->render('authentication/sign_in.html.twig', [
             'current_menu' => 'sign_in',
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/signout", name="sign_out")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function signout(AuthenticationUtils $authenticationUtils)
+    {
+
+        return $this->render('home.html.twig', [
+            'current_menu' => 'home'
         ]);
     }
 }
---
+++ src/Form/SignUpType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SignUpType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email', EmailType::class)
+            ->add('birthDate', DateTimeType::class)
+            ->add('newsletter', CheckboxType::class)
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ))
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
